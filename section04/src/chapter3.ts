97a1c1d4aafd616817bb5b4f6138aec3832e3b60
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-03-05 10:37:58 +0900

    함수 오버로딩

---
+++ section04/src/chapter3.ts
@@ -0,0 +1,17 @@
+/*함수 오버로딩*/
+//하나의 함수 func
+// -> 모든 매개변수의 타입 number
+// -> Ver1. 매개변수가 1개 -> 이 매개변수에 20을 곱한 값 출력
+// -> Ver2. 매개변수가 3개 -> 이 매개변수들을 다 더한 값을 출력
+
+// 버전들 = 오버로드 시그니쳐
+function func(a: number): void;
+function func(a: number, b: number, c: number): void;
+
+// 실제 구현부 = 구현 시그니쳐
+function func(a: number, b?: number, c?: number) {}
+
+// func();
+func(1);
+// func(1, 2);
+func(1, 2, 3);
