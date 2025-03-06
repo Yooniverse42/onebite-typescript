e47321f5217152d9331090af27ca940340a725cf
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-03-06 16:47:10 +0900

    제네릭 인터페이스

---
+++ section07/src/chapter3.ts
@@ -0,0 +1,38 @@
+/*제네릭 인터페이스*/
+
+interface KeyPair<K, V> {
+  key: K;
+  value: V;
+}
+
+let keyPair: KeyPair<string, number> = {
+  key: "key",
+  value: 0,
+};
+
+let keyPair2: KeyPair<boolean, string[]> = {
+  key: true,
+  value: ["1"],
+};
+
+// 인덱스 시그니처
+interface Map<V> {
+  [key: string]: V;
+}
+
+let stringMap: Map<string> = {
+  key: "value",
+};
+
+let booleanMap: Map<boolean> = {
+  key: true,
+};
+
+// 제네릭 타입 별칭
+type Map2<V> = {
+  [key: string]: V;
+};
+
+let stringMap2: Map2<string> = {
+  key: "hello",
+};
